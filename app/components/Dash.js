396cdf4f40c23b448ad24c4a924742d8f9a7de19
Matthew Krager <metoofarmer@yahoo.com>
2017-01-03 19:36:20 -0800

    tabs don't work

--- app/components/Dash.js
+++ app/components/Dash.js
@@ -1,27 +1,57 @@
-import React, {Component, PropTypes} from 'react'
+import React from 'react';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
 const styles = {
-  root: {
-    height: "calc(100vh - 180px)"
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
   },
-  header: {
-    height: 16,
-    marginLeft: 16
-  }
 };
 
-class Dash extends Component {
+export default class Dash extends React.Component {
 
-  componentDidMount() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'a',
+    };
   }
+
+  handleChange = (value) => {
+    this.setState({
+      value: value,
+    });
+  };
 
   render() {
     return (
-      <div style={styles.root}>
-        <h3 style={styles.header}>Dashboard</h3>
-      </div>
+      <Tabs
+        value={this.state.value}
+        onChange={this.handleChange}
+      >
+        <Tab label="Tab A" value="a" >
+          <div>
+            <h2 style={styles.headline}>Controllable Tab A</h2>
+            <p>
+              Tabs are also controllable if you want to programmatically pass them their values.
+              This allows for more functionality in Tabs such as not
+              having any Tab selected or assigning them different values.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="Tab B" value="b">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>
+      </Tabs>
     );
   }
 }
-
-export default Dash;
